bfaa6b8ecd99871697734d1a08a94c08cb2bf244
Ayushman Singh Sisodiya <ayushmanss@juniper.net>
2019-10-03 03:12:50 +0530

    dockerfile and build process for client

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 import requests
 
@@ -7,11 +8,15 @@ def main():
     proxy_server_port = 8080
     url = "http://localhost:{}/".format(proxy_server_port)
     while True:
-        start_time = random.randint(1, 101)
-        duration = random.randint(100, 10001)
-        querystring = {"service": "database_server", "start_time": str(start_time), "duration": str(duration)}
-        response = requests.request("GET", url, params=querystring)
-        print(response.text)
+        try:
+            start_time = random.randint(1, 101)
+            duration = random.randint(100, 10001)
+            querystring = {"service": "database_server", "start_time": str(start_time), "duration": str(duration)}
+            response = requests.request("GET", url, params=querystring)
+            print(response.text)
+        except requests.exceptions.RequestException:
+            print('connection error, retrying after 5 seconds')
+            time.sleep(5)
 
 if __name__ == "__main__":
     main()
